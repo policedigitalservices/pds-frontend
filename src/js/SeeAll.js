657294f2338e48f5d36a72a5b9d239a9ca2f0d7e
rb8713 <robert.bowen@avonandsomerset.police.uk>
2020-07-01 16:59:33 +0100

    Refactor seeall.js.  'See all' buttons need attribute data-showall-table-id="****"  and optional data-showall-item-limit="10" (default 10)

--- src/js/SeeAll.js
+++ src/js/SeeAll.js
@@ -1,6 +1,40 @@
+var showMoreTableButtons = document.querySelectorAll('button[data-showall-table-id]');
+
+showMoreTableButtons.forEach(showAllButton => {
+  var tableId = showAllButton.getAttribute("data-showall-table-id");
+  var defaultItemLimit = 10;
+
+  var table = document.getElementById(tableId);
+  if (table) {
+    var passedItemLimit = showAllButton.getAttribute("data-showall-item-limit");
+    var parsedItemLimit = parseInt(passedItemLimit, 10);
+    var limitToUse = isNaN(parsedItemLimit) ? defaultItemLimit : parsedItemLimit;
+    var tableLength = table.rows.length;
+
+    for (var i = 1, row; row = table.rows[i]; i++) {
+      if (i > limitToUse) {
+        row.style.display = "none";
+      }
+    }
+
+    if (tableLength <= (limitToUse+1)) { showAllButton.style.display = "none"; }
+
+    showAllButton.addEventListener('click', (el) => {
+      el.preventDefault();
+
+      Array.from(table.rows).forEach(function(row) {
+        row.style.display = "table-row";
+      });
+    });
+  }
+});
+
+/*
+console.log('showMoreTableButtons', showMoreTableButtons);
+
 if (document.getElementById("messageStatuses") != null) {
   (function(selector) {
-    var table = document.getElementById("messageStatuses");
+    var table = document.getElementById(selector);
     for (var i = 0, row; (row = table.rows[i]); i++) {
       if (i >= 11) {
         row.style.display = "none";
@@ -12,7 +46,7 @@ if (document.getElementById("messageStatuses") != null) {
     }
     seeAllStatusesButton.addEventListener("click", function(el) {
       el.preventDefault();
-      var table = document.getElementById("messageStatuses");
+      var table = document.getElementById("selector");
       for (var i = 0, row; (row = table.rows[i]); i++) {
         row.style.display = "table-row";
       }
@@ -22,7 +56,7 @@ if (document.getElementById("messageStatuses") != null) {
 
 if (document.getElementById("messageResponses") != null) {
   (function(selector) {
-    var table = document.getElementById("messageResponses");
+    var table = document.getElementById(selector);
     for (var i = 0, row; (row = table.rows[i]); i++) {
       if (i >= 11) {
         row.style.display = "none";
@@ -34,10 +68,11 @@ if (document.getElementById("messageResponses") != null) {
     }
     seeAllStatusesButton.addEventListener("click", function(el) {
       el.preventDefault();
-      var table = document.getElementById("messageResponses");
+      var table = document.getElementById(selector);
       for (var i = 0, row; (row = table.rows[i]); i++) {
         row.style.display = "table-row";
       }
     });
   })("messageResponses");
 }
+*/
