50b10e6069364408ddc315493c204d86d245a213
rb8713 <robert.bowen@avonandsomerset.police.uk>
2020-08-18 08:37:19 +0100

    Populate the new force users table rows

--- src/js/pages/SelectStaffForMessage.js
+++ src/js/pages/SelectStaffForMessage.js
@@ -11,7 +11,7 @@ if (main && main.classList.contains('asc-staff-index')) {
 
   const ch = new IdCookieHelper('CourierMessageUserIds');
   const dmd = new DraftMessageDrawer(ch.getCount());
-  const loader = document.querySelector('loader');
+  const loader = document.querySelector('.loader');
 
   const usersTable = document.getElementById('table-select-staff');
   let usersTableBody;
@@ -32,15 +32,47 @@ if (main && main.classList.contains('asc-staff-index')) {
     }
   });
 
+  const createCheckboxField = (value, checked) => {  
+    const cell = document.createElement('td');
+    cell.classList.add('checkbox-cell');
+    const label = document.createElement('label');
+    label.classList.add('checkbox');
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.value = value;
+    input.checked = checked;
+    const span = document.createElement('span');
+    label.appendChild(input);
+    label.appendChild(span);
+    cell.appendChild(label);
+    return cell;
+  }
+
+  const createCellWithText = text => {
+    const cell = document.createElement('td');
+    cell.innerText = "Put name here";
+    return cell;
+  }
+
   const addRow = user => {
     const newRow = document.createElement('tr');
-    // TODO: add tds into this row, using 'user' details.
+
+    newRow.appendChild(createCellWithText('PUT NAME HERE FROM USER'));
+    newRow.appendChild(createCellWithText('PUT COLLAR HERE FROM USER'));
+    newRow.appendChild(createCellWithText('PUT EMAIL HERE FROM USER'));
+    newRow.appendChild(createCellWithText('PUT PHONE HERE FROM USER'));
+    newRow.appendChild(createCheckboxField('PUT COLLAR HERE', ch.hasId('PUT COLLAR HERE')));
+
     usersTableBody.append(newRow);
   }
 
   const addResultRows = rows => {
     rows.forEach(addRow);
   }
+
+  /*
+
+  // TODO: Restore this block of code once the end point is in place.
 
   if (loader) {
     new LazyLoader(loader, async (done) => {
@@ -55,16 +87,18 @@ if (main && main.classList.contains('asc-staff-index')) {
         const json = await response.json();
 
         addResultRows(json.rows);
-        done(json.hasMoreRows);
+        done(!json.hasMoreRows);
       }
       catch(e) {        
         console.error(`Failed to lazy load page ${currentPage} of AD users`);
         console.error(e);
-        // Ignore current failed page
+        // Ignore current failed page, so will try again
         currentPage--;
-        done(false);
+        done(true); // Call done with more to load.
+        // TODO: What to do here. If enpoint is down, we would get lots of console errors at the moment....
       }
       
     }, { debug: false, peekDistance: 50 });
   }  
+  */
 }
