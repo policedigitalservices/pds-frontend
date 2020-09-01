97762f787ce05c9b9aa8715725851f14ded5b235
rb8713 <robert.bowen@avonandsomerset.police.uk>
2020-09-01 11:25:26 +0100

    Persist auto resend option

--- src/js/pages/ComposeStaffMessage.js
+++ src/js/pages/ComposeStaffMessage.js
@@ -13,14 +13,17 @@ if (main && main.classList.contains('asc-staff-compose')) {
   const addBtn = document.getElementById('add_contact');
   const subjectInput = document.getElementById('subject_input');
   const messageInput = document.getElementById('Input_Message');
+  const resendInput = document.getElementById('Input_AutoResendSchedule');
 
   const cookieHelper = new SimpleCookieHelper('CourierMessageTitle');
   const cookieMessageHelper = new SimpleCookieHelper('CourierMessageContent');
+  const autoResendHelper = new SimpleCookieHelper('CourierMessageResend');
   const idsCookieHelper = new IdCookieHelper('CourierMessageUserIds');
 
   addBtn.addEventListener('click', () => {
     cookieHelper.set(subjectInput.value);
     cookieMessageHelper.set(messageInput.value);
+    autoResendHelper.set(resendInput.value)
   });
   
   const onUpdateFunc = (selectedOptions, allOptions) => {
