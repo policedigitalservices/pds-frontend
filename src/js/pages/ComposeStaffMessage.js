add7823ba2445a02263f1dd59eb3a58190811491
rb8713 <robert.bowen@avonandsomerset.police.uk>
2020-09-01 11:06:01 +0100

    Persist message content in cookie on force compose page

--- src/js/pages/ComposeStaffMessage.js
+++ src/js/pages/ComposeStaffMessage.js
@@ -12,12 +12,15 @@ if (main && main.classList.contains('asc-staff-compose')) {
 
   const addBtn = document.getElementById('add_contact');
   const subjectInput = document.getElementById('subject_input');
-  
+  const messageInput = document.getElementById('Input_Message');
+
   const cookieHelper = new SimpleCookieHelper('CourierMessageTitle');
+  const cookieMessageHelper = new SimpleCookieHelper('CourierMessageContent');
   const idsCookieHelper = new IdCookieHelper('CourierMessageUserIds');
 
   addBtn.addEventListener('click', () => {
     cookieHelper.set(subjectInput.value);
+    cookieMessageHelper.set(messageInput.value);
   });
   
   const onUpdateFunc = (selectedOptions, allOptions) => {
