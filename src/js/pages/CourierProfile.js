0ef2ac6f01e4369709bed1345a0af59030685d63
rb8713 <robert.bowen@avonandsomerset.police.uk>
2020-09-03 08:15:48 +0100

    Refactor

--- src/js/pages/CourierProfile.js
+++ src/js/pages/CourierProfile.js
@@ -1,19 +1,8 @@
-const main = document.querySelector('main');
+import CheckboxDisableButtonHelper from '../CheckboxDisableButtonHelper';
 
-if (main && main.classList.contains('courier-profile-page')) {
+// Check we are on the correct page.
+const main = document.querySelector('main.courier-profile-page');
 
-    // Capture and disable the save button.
-    const saveButton = document.getElementById('btn-save');
-    saveButton.setAttribute('disabled', 'diabled');
-
-    // Capture and ensure accept terms checkbox is deselected
-    const cbAcceptTerms = document.getElementById('cbAcceptTerms');
-    cbAcceptTerms.checked = false;
-
-    // Add and event listener that enables and disables the button.
-    cbAcceptTerms.addEventListener('change', (e) => {
-        e.target.checked ?
-            saveButton.removeAttribute('disabled') :
-            saveButton.setAttribute('disabled', 'disabled');
-    });
+if (main) {
+    new CheckboxDisableButtonHelper('#btnSave', '#cbAcceptTerms');
 }
