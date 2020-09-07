cea9936fc3d98ce5c595c5706f1726ebd74948a7
rb8713 <robert.bowen@avonandsomerset.police.uk>
2020-09-07 11:03:49 +0100

    Add buttons and listeners to adding and removing emails and phone numbers (WIP)

--- src/js/pages/CourierProfile.js
+++ src/js/pages/CourierProfile.js
@@ -4,5 +4,42 @@ import CheckboxDisableButtonHelper from '../CheckboxDisableButtonHelper';
 const main = document.querySelector('main.courier-profile-page');
 
 if (main) {
-    new CheckboxDisableButtonHelper('#btnSave', '#cbAcceptTerms');
+    
+    //new CheckboxDisableButtonHelper('#btnSave', '#cbAcceptTerms');
+
+    // Grab all the buttons that are only show when js enabled
+    const addPhoneBtn = document.getElementById('addPhoneBtn');
+    const addEmailBtn = document.getElementById('addEmailBtn');
+    const removeEmailButtons = Array.from(document.querySelectorAll('.btn-remove-contact-email'));
+    const removePhoneButtons = Array.from(document.querySelectorAll('.btn-remove-contact-phone'));
+    
+    // Get the form, to add remove listeners to
+    const contactProfileForm = document.getElementById('contact-profile-form');
+
+    // Show all the js only buttons
+    [addPhoneBtn, addEmailBtn, ...removeEmailButtons, ...removePhoneButtons].forEach(el => {
+        el.style.display = 'inline-block';
+    });
+
+    // Wire up the button listeners
+    addPhoneBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        alert('Add phone Button clicked');
+    });
+    addEmailBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        alert('Add email Button clicked');
+    });  
+
+    // Handle the remove on the parent form, so that will work for new items added after load.
+    contactProfileForm.addEventListener('click', (e) => {
+        if (e.target.matches('.btn-remove-contact-phone')  || e.target.matches('.btn-remove-contact-email')) {
+            // Remove parent form group containing email or phone.
+            const formGroup = e.target.closest('.form__group');
+            if (formGroup) {
+                formGroup.remove();
+            }
+        }
+    });
+
 }
