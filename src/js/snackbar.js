4c9e85d94a72bfd59c9e651f817f9be03b843308
rb8713 <robert.bowen@avonandsomerset.police.uk>
2020-09-24 13:19:19 +0100

    Spacing tweaks. Remove commented out code.  Prevent reshowing snackbar messages on refresh

--- src/js/snackbar.js
+++ src/js/snackbar.js
@@ -4,24 +4,35 @@
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
-    var contactParam = urlParams.get('ContactAdded');
-    var OrganisationParam = urlParams.get('OrganisationAdded');
-    var MessageSentParam = urlParams.get('MessageSent');
-    var ForceContactUpdatedParam = urlParams.get('ForceContactUpdated');
+    const removeQueryParam = param => {
+        if(urlParams.has(param)){
+            urlParams.delete(param);            
+            window.history.replaceState({}, document.title, `${window.location.pathname}?=${urlParams.toString()}`);
+        }
+    }
 
-    var snackbarMessage = '';
+    const contactParam = urlParams.get('ContactAdded');
+    const OrganisationParam = urlParams.get('OrganisationAdded');
+    const MessageSentParam = urlParams.get('MessageSent');
+    const ForceContactUpdatedParam = urlParams.get('ForceContactUpdated');
+
+    let snackbarMessage = '';
 
     if (contactParam === 'True') {
+        removeQueryParam('ContactAdded');
         snackbarMessage = 'New contact has been created';
     } else if (OrganisationParam === 'True') {
+        removeQueryParam('OrganisationAdded');
         snackbarMessage = 'New organisation has been created';
     } else if (MessageSentParam === 'True') {
         /* 
             Bit hacky, but clearing the sessionStorage here to clear message state here. 
         */
         sessionStorage.removeItem("CourierMessageUsers")
+        removeQueryParam('MessageSent');
         snackbarMessage = 'Message sent';
     } else if (ForceContactUpdatedParam && ForceContactUpdatedParam.toLowerCase() === 'true') {
+        removeQueryParam('ForceContactUpdated');
         snackbarMessage = 'Personal contact details updated';
     } 
 
