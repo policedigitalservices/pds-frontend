7d0f51072c41101a541177ad851edb5ae7e06a0a
Christian King <christian@madebyck.com>
2020-12-14 13:49:37 +0000

    swap to older version of master

--- webpack.config.js
+++ webpack.config.js
@@ -6,33 +6,21 @@ const path = require("path");
 const sass = require("sass");
 const webpack = require("webpack");
 
-const outputDir = path.resolve(__dirname, "dist");
+const outputDir = path.resolve(__dirname, "");
 const isDev = process.env.NODE_ENV !== "production";
 const outputEnv = isDev ? "." + process.env.NODE_ENV : "";
 
-function recursiveIssuer(m) {
-  if (m.issuer) {
-    return recursiveIssuer(m.issuer);
-  } else if (m.name) {
-    return m.name;
-  } else {
-    return false;
-  }
-}
-
 module.exports = {
   entry: {
-    "styles": ["./src/scss/styles.scss"],
-    "scripts": [
+    "dist/css/styles": ["./src/scss/styles.scss"],
+    "dist/js/scripts": [
       "./src/js/Start.js",
-      "./src/scss/styles.scss",
       "./src/js/GroupsTree.js",
       "./src/js/side.js",
       "./src/js/ToggleContent.js",
       "./src/js/ToggleSidebar.js",
       "./src/js/tables.js",
       "./src/js/SeeAll.js",
-      "./src/js/showPassword.js",
       "./src/js/menu.js",
       "./src/js/snackbar.js",
       "./src/js/hint-snackbar.js",
@@ -41,47 +29,16 @@ module.exports = {
       "./src/js/search.js",
       "./src/js/pages/SelectStaffForMessage.js",
       "./src/js/pages/ComposeStaffMessage.js",
-      "./src/js/pages/CourierProfile.js",
-      "./src/js/messagesView.js"
-    ],
-    courier: [
-      "./src/scss/overrides/courier.scss"
-    ],
-    integrity: [
-      "./src/scss/overrides/integrity.scss"
+      "./src/js/pages/CourierProfile.js"
     ]
   },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        courierStyles: {
-          name: 'courier',
-          test: (m, c, entry = 'courier') =>
-            m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
-          chunks: 'all',
-          enforce: true,
-        },
-        integrityStyles: {
-          name: 'integrity',
-          test: (m, c, entry = 'integrity') =>
-            m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
-          chunks: 'all',
-          enforce: true,
-        },
-      },
-    },
-  },
   output: {
-    filename: "js/[name]" + outputEnv + ".js",
+    filename: "[name]" + outputEnv + ".js",
     // filename: isDev ? "[name].js" : "[name].[contenthash].js",
     path: outputDir
   },
   devtool: isDev ? "inline-source-map" : false,
   devServer: {
-    headers: {
-    "Access-Control-Allow-Origin": "*",
-    https: true
-  },
     contentBase: outputDir,
     compress: isDev ? true : false,
     port: 7264
@@ -89,12 +46,13 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin([outputDir]),
     new MiniCssExtractPlugin({
-      filename: "css/[name]" + outputEnv + ".css",
+      filename: "[name]" + outputEnv + ".css"
+      // filename: isDev ? "[name].css" : "[name].[contenthash].css"
     }),
     new CopyWebpackPlugin([
-      { from: "./src/img", to: "img" },
-      { from: "./src/fonts", to: "fonts" },
-      { from: "./src/lib", to: "lib" }
+      { from: "./src/img", to: "dist/img" },
+      { from: "./src/fonts", to: "dist/fonts" },
+      { from: "./src/lib", to: "dist/lib" }
     ]),
     new webpack.ProvidePlugin({
       $: "jquery",
@@ -148,11 +106,11 @@ module.exports = {
       {
         test: /\.svg$/,
         loader: "file-loader"
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: "file-loader"
       }
+      // {
+      //   test: /\.(woff|woff2|eot|ttf|otf)$/,
+      //   loader: "file-loader"
+      // }
     ]
   }
 };
